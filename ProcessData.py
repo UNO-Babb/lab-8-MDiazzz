4e9b2180de57572e25b95ff6321f4b5a98a76f45
MDiazzz <michellediazrn@gmail.com>
2025-10-28 20:34:41 +0000

    Commit

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Michelle Diaz
+#Date: 10/28/2025
+#Assignment: Process Data
 
 import random
 
@@ -12,12 +12,31 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+   
 
+   for line in inFileFile:
+    info = line.split(",")
+    first_name = info [0]
+    last_name = info [1]
+    user_ID = info [] 
+    major_year = info []
+    
+  OutFile = open("names.dat",'w')
 
+    for line in team_data:
+    output = line[0] + "," + line[1] + "," + line[2] + "," + line[3] + "," + line[4] + "," +line[5] + "," + "\n"
+    OutFile.write(str(line))
+
+OutFile.close() 
+
+ myFile.close()
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
 
+
+if __name__ == '__main__':
+  main()
 if __name__ == '__main__':
   main()
