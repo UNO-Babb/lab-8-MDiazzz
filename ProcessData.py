97dfaba7bb0f147f10f5ae7a0035d00a814f1099
MDiazzz <michellediazrn@gmail.com>
2025-10-29 23:37:13 +0000

    Commit

--- ProcessData.py
+++ ProcessData.py
@@ -1,42 +1,49 @@
 #ProcessData.py
 #Name: Michelle Diaz
-#Date: 10/28/2025
+#Date: 10/29/2025
 #Assignment: Process Data
 
-import random
+def makeID(first_name, last_name, student_id):
+  user_id = first_name[0]  # first initial
+
+  user_id += last_name
+
+  # if last name less than 5 characters long, fill with 'X' until it's 5 characters long
+  if len(last_name) < 5:
+    user_id += 'X' * (5 - len(last_name))
+
+  user_id += student_id[-3:]  # add last 3 characters from student id #
+
+  return user_id
 
 def main():
+
+  abbreviateYear = {"Freshman": "FR", "Sophomore": "SO", "Junior": "JR", "Senior": "SR"}
 
   #Open the files we will be using
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
-   
+  for line in inFile:
+    # First name	Last Name	E-Mail	Student ID	Date of Birth	Year	Major
 
-   for line in inFileFile:
-    info = line.split(",")
-    first_name = info [0]
-    last_name = info [1]
-    user_ID = info [] 
-    major_year = info []
-    
-  OutFile = open("names.dat",'w')
+    info = line.split()
+    first_name = info[0]
+    last_name = info[1]
+    student_id = info[3]
+    year = info[5]
+    major = info[6]
 
-    for line in team_data:
-    output = line[0] + "," + line[1] + "," + line[2] + "," + line[3] + "," + line[4] + "," +line[5] + "," + "\n"
-    OutFile.write(str(line))
+    user_id = makeID(first_name, last_name, student_id)
+    major_year = major[:3] + '-' + abbreviateYear[year]
 
-OutFile.close() 
-
- myFile.close()
+    outFile.write(first_name + ',' + last_name + ',' + user_id + ',' + major_year + '\n')
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
 
 
-if __name__ == '__main__':
-  main()
 if __name__ == '__main__':
   main()
